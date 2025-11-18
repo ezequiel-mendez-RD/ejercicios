714af4a2db488cfe24203f53213cd7dd259154a6
ezequiel-mendez-RD <ezequielelpro20@gmail.com>
2025-11-18 09:48:01 +0100

    commit 3

--- ejercicio2.py
+++ ejercicio2.py
@@ -3,3 +3,10 @@ def fact(n):
     if n < 1:
         return 1
     return n * fact(n-2)
+try:
+    if n <= 0:
+        raise ValueError
+    p = fact(n)
+    print(p)
+except ValueError:
+    print("Es un número negativo")
