dd3dc49cd7a2df3479a73dc3eba6b0482a24a43d
ezequiel-mendez-RD <ezequielelpro20@gmail.com>
2025-11-20 10:39:20 +0100

    commit 1

---
+++ ejercicio3.py
@@ -0,0 +1,7 @@
+def leibniz_pi(N: int) -> float:
+    suma = 0.0
+    signo = 1.0
+    for n in range(N):
+        suma += signo / (2 * n + 1)
+        signo = -signo  # alterna +1, -1
+    return 4.0 * suma
