9a57d53b63aaf5c4b29c84c471084338b3ce0600
ezequiel-mendez-RD <ezequielelpro20@gmail.com>
2025-11-20 10:40:20 +0100

    commit 2

--- ejercicio3.py
+++ ejercicio3.py
@@ -5,3 +5,7 @@ def leibniz_pi(N: int) -> float:
         suma += signo / (2 * n + 1)
         signo = -signo  # alterna +1, -1
     return 4.0 * suma
+
+if __name__ == "__main__":
+    N = 2_000_000
+    aproximacion = leibniz_pi(N)
